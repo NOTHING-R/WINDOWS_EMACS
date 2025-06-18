b0b8cd9dbef810c84361db31b246c032b817efc6
NOTHING-R <kalilinux.nothing@gmail.com>
2025-06-18 22:30:59 +0600

    Emacs Config for windows

---
+++ init.el
@@ -0,0 +1,22 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(package-selected-packages
+   '(all-the-icons-ivy-rich ccls company counsel dashboard dired-open
+			    doom-modeline doom-themes evil-collection
+			    evil-tutor flycheck general lsp-pyright
+			    org-bullets org-noter peep-dired
+			    python-mode shell-toggle sudo-edit toc-org)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
